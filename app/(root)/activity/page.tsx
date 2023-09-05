185206929587da935c03d49416cab18ae3962305
MahmoudEMan <69572998+MahmoudEMan@users.noreply.github.com>
2023-09-05 04:05:56 +0300

    Update page.tsx

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { formatDateString } from "@/lib/utils";
 
 import { fetchUser, getActivity } from "@/lib/actions/user.action";
 
@@ -19,25 +20,32 @@ async function Page() {
     <>
       <h1 className="head-text">Activity</h1>
 
-      <section className="mt-10 flex flex-col gap-5">
+      <section className="mt-10 flex flex-col-reverse gap-5">
         {activity.length > 0 ? (
           <>
             {activity.map((activity) => (
               <Link key={activity._id} href={`/thread/${activity.parentId}`}>
-                <article className="activity-card">
-                  <Image
-                    src={activity.author.image}
-                    alt="user_logo"
-                    width={20}
-                    height={20}
-                    className="rounded-full object-cover"
-                  />
-                  <p className="!text-small-regular text-light-1">
-                    <span className="mr-1 text-primary-500">
-                      {activity.author.name}
-                    </span>{" "}
-                    replied to your thread
-                  </p>
+                <article className="activity-card justify-between">
+                  <div className="flex gap-4">
+                    <Image
+                      src={activity.author.image}
+                      alt="user_logo"
+                      width={20}
+                      height={20}
+                      className="rounded-full object-cover"
+                    />
+                    <p className="!text-small-regular text-light-1">
+                      <span className="mr-1 text-primary-500">
+                        {activity.author.name}
+                      </span>{" "}
+                      replied to your thread
+                    </p>
+                  </div>
+
+                  <div className="text-[#bcbcbc]">
+                    {" "}
+                    {formatDateString(activity.createdAt)}
+                  </div>
                 </article>
               </Link>
             ))}
